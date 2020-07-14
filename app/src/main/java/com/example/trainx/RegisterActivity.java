06bcecc67bd4982dfec3ce92c3f0666313f099ff
Mateusz Migot <migotmateusz@gmail.com>
2020-07-14 19:10:47 +0200

    login screen finished

--- app/src/main/java/com/example/trainx/RegisterActivity.java
+++ app/src/main/java/com/example/trainx/RegisterActivity.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
     }
 }
