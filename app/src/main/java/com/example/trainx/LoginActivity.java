2ce7e7831a7a5bb7b836bbe688569c6f2e5c4821
Mateusz Migot <migotmateusz@gmail.com>
2020-07-14 19:57:08 +0200

    Register screen added

--- app/src/main/java/com/example/trainx/LoginActivity.java
+++ app/src/main/java/com/example/trainx/LoginActivity.java
@@ -2,7 +2,12 @@ package com.example.trainx;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.button.MaterialButton;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,5 +16,14 @@ public class LoginActivity extends AppCompatActivity {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button button = findViewById(R.id.SignUpButton);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.pull_in_left, R.anim.pull_out_right);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/trainx/RegisterActivity.java
+++ app/src/main/java/com/example/trainx/RegisterActivity.java
@@ -2,13 +2,26 @@ package com.example.trainx;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        Button button = findViewById(R.id.LogMeInButton);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.pull_in_right, R.anim.pull_out_left);
+            }
+        });
     }
 }
