213ce12d4685b36bae9916b5feb053ca3b53f91c
Mateusz Migot <migotmateusz@gmail.com>
2020-07-23 14:08:03 +0200

    added Week Training Screen

--- app/src/main/java/com/example/trainx/MainActivity.java
+++ app/src/main/java/com/example/trainx/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.trainx;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toolbar;
 
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,7 +20,26 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         tabLayout = (TabLayout)findViewById(R.id.TopTabLayout);
+        TabItem tabWeek = findViewById(R.id.TWeekTab);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                ThisWeekFragment thisWeekFragment = new ThisWeekFragment();
+                thisWeekFragment.setArguments(getIntent().getExtras());
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.FrameLayout, thisWeekFragment).commit();
+            }
 
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
 
     }
 }
